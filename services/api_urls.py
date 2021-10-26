5b2dc3fd5402f672bae6476c78b964e1650a13fb
Silvian <silvian.dragan@gmail.com>
2021-10-26 13:26:45 +0100

    implemented the services list devices api endpoint

--- services/api_urls.py
+++ services/api_urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
 app_name = "services"
 
-urlpatterns = [
+router = DefaultRouter()
+
+router.register(r"list-devices", views.ListDevicesViewSet, basename="devices")
+
+urlpatterns = router.urls
+urlpatterns += [
     path('webhook/', views.WebhookAPIView.as_view(), name='webhook'),
 ]
