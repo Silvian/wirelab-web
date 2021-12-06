05cf9c8de0911ec3de34168a36838658b386dde0
Silvian <silvian.dragan@gmail.com>
2021-12-06 20:58:09 +0000

    added accounts integration tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.factories import UserFactory, StaffUserFactory, AdminUserFactory
+
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.staff_user = StaffUserFactory()
+        self.admin_user = AdminUserFactory()
+
+    def test_authenticate_user(self):
+        data = {
+            "email": self.user.email,
+            "password": self.user.password,
+        }
+        response = self.client.post("/accounts/authenticate/", data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_user(self):
+        self.client.force_login(user=self.user)
+        response = self.client.get("/accounts/logout")
+        self.assertEqual(response.url, "/accounts/logout/")
+        self.assertEqual(response.status_code, 301)
