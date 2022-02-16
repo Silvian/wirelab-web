09f314a92702f9c70ac246695c38f5942332b9b1
Silvian <silvian.dragan@gmail.com>
2022-02-16 19:12:34 +0000

    created custom actions to enable and disable auto mode

--- devices/admin.py
+++ devices/admin.py
@@ -1,10 +1,16 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
 
 from .models import Device
 
 
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
+
+    actions = (
+        'enable_auto',
+        'disable_auto',
+    )
 
     list_display = (
         'name',
@@ -34,3 +40,21 @@ class DeviceAdmin(admin.ModelAdmin):
     filter_horizontal = (
         'owners',
     )
+
+    @admin.action(description='Enable auto for selected devices')
+    def enable_auto(self, request, queryset):
+        enabled = queryset.update(auto=True)
+        self.message_user(request, ngettext(
+            'Enabled auto mode based on sunset for %d device.',
+            'Enabled auto mode based on sunset for %d devices.',
+            enabled,
+        ) % enabled, messages.SUCCESS)
+
+    @admin.action(description='Disable auto for selected devices')
+    def disable_auto(self, request, queryset):
+        disabled = queryset.update(auto=False)
+        self.message_user(request, ngettext(
+            'Disabled auto mode based on sunset for %d device.',
+            'Disabled auto mode based on sunset for %d devices.',
+            disabled,
+        ) % disabled, messages.SUCCESS)
